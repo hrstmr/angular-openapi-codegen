1a6ff99746831cbb90aa448c4a7ecfa369d2e523
hrstmr <37159938+hrstmr@users.noreply.github.com>
2024-01-07 13:24:25 +1100

    get paths workign

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { operations, paths } from '../../petstore';
+
+const paths: paths = null!;
+const storeOrder = paths['/store/order/{orderId}'];
+type StoreOrderVerbs = keyof typeof storeOrder;
+type StoreOrder = 'get' extends StoreOrderVerbs ? 'yes' : 'no';
+
+type pathStrings = keyof paths;
+type hasGetVerb<verb, path> = 'get' extends verb ? path : verb;
+type hasGet<T extends keyof paths> = T extends T
+    ? 'get' extends keyof paths[T]
+        ? T
+        : never
+    : never;
+
+type getPaths = sd;
+
+const getPaths: getPaths = '/store/order';
+
+type x = keyof StoreOrder extends 'post' ? 'yes' : 'no';
+type gets = hasGet<pathStrings>;
+
+const x: gets = '/store/order/{orderId}';
 
 @Component({
     selector: 'app-root',
@@ -10,5 +34,10 @@ import { RouterOutlet } from '@angular/router';
     styleUrl: './app.component.scss',
 })
 export class AppComponent {
+    http = inject(HttpClient);
     title = 'angular-openapi-codgen';
+
+    abc() {}
+
+    test(paths: paths, operations: operations) {}
 }
