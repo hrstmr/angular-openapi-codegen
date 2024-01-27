9ad718aa333fdaee8ebc6f8926701a756af2d0b9
hrstmr <37159938+hrstmr@users.noreply.github.com>
2024-01-27 15:56:32 +1100

    up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { PetApiService } from './pet-api.service';
 import { HttpClient } from '@angular/common/http';
+import { paths } from '../../petstore';
+
+const baseUrl = 'https://petstore3.swagger.io/api/v3' as const;
 
 @Component({
     selector: 'app-root',
@@ -14,10 +17,57 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent {
     api = inject(PetApiService);
+    http = inject(HttpClient);
     title = 'angular-openapi-codegen';
 
     abc() {
         this.test();
+    }
+
+    getStoreInventory1() {
+        return this.api.get('/store/inventory', {});
+    }
+
+    getStoreInventory2() {
+        const path = '/store/inventory' as const;
+        return this.http.get<
+            paths[typeof path]['get']['responses']['200']['content']['application/json']
+        >(baseUrl + path);
+    }
+
+    getStoreOrderById1(orderId: number) {
+        return this.api.get('/store/order/{orderId}', {
+            pathParams: { orderId },
+        });
+    }
+
+    getStoreOrderById2({ orderId }: paths['/store/order/{orderId}']['get']['parameters']['path']) {
+        const path = `/store/order/${orderId}` as const;
+
+        return this.http.get<
+            paths['/store/order/{orderId}']['get']['responses']['200']['content']['application/json']
+        >(baseUrl + path);
+    }
+
+    UserLogin1(password: string, username: string) {
+        return this.api.get('/user/login', {
+            queryParams: {
+                password,
+                username,
+            },
+        });
+    }
+
+    UserLogin2(password: string, username: string) {
+        const path = `/user/login` as const;
+        return this.http.get<
+            paths['/user/login']['get']['responses']['200']['content']['application/json']
+        >(baseUrl + path, {
+            params: {
+                password,
+                username,
+            } as paths['/user/login']['get']['parameters']['query'],
+        });
     }
 
     test() {
